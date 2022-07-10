4253a81555a1e56fa8693c8e3f29aa5216603733
Leo <balashov.leo@gmail.com>
2022-07-10 09:58:02 +0300

    Demo movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,23 +7,32 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField, Range(0, 7)] private float Speed;
     private Transform _transform;
     private Rigidbody2D _rigidbody;
+    private float _radius;
 
     void Start()
     {
         _transform = GetComponent<Transform>();
         _rigidbody = GetComponent<Rigidbody2D>();
+        _radius = GetComponent<CircleCollider2D>().radius;
     }
 
     void Update()
     {
         Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"),
                                             Input.GetAxis("Vertical"));
-        movementVector *= Time.deltaTime;
-        float raycastDistance = movementVector.magnitude + (_transform.localScale.x / 2f);
+        movementVector *= Time.deltaTime * Speed;
+        float raycastDistance = movementVector.magnitude + _radius;
 
-        Physics2D.Raycast(origin: _transform.position,
-                            direction: movementVector,
-                            distance: raycastDistance,
-                            layerMask: Physics2D.GetLayerCollisionMask(gameObject.layer));
+        RaycastHit2D hit = Physics2D.Raycast(origin: _transform.position,
+                                            direction: movementVector,
+                                            distance: raycastDistance,
+                                            layerMask: Physics2D.GetLayerCollisionMask(gameObject.layer));
+        if (hit)
+        {
+            float movementMaxDistance = Vector2.Distance(_transform.position, hit.point) - _radius;
+            movementVector = Vector2.ClampMagnitude(movementVector, movementMaxDistance);
+        }
+
+        _transform.Translate(movementVector);
     }
 }
