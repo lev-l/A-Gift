fd06ecff3d89e60593f7318ba05e445da8168699
Leo <balashov.leo@gmail.com>
2022-07-11 07:56:36 +0300

    Working bow prototype

--- Assets/Scripts/Bow.cs
+++ Assets/Scripts/Bow.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Range(10, 100)] private float _maxEnergy;
+    private float _currentEnergy = 0;
+    private bool _charging = false;
+    private Transform _transform;
+    private int _layerMask;
+
+    private void Start()
     {
-        
+        _transform = GetComponent<Transform>();
+        _layerMask = Physics2D.GetLayerCollisionMask(_transform.parent.gameObject.layer);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            _charging = true;
+        }
+        if (_charging
+            && Input.GetMouseButtonUp(0))
+        {
+            _charging = false;
+            Fire();
+        }
+
+        if (_charging)
+        {
+            _currentEnergy += 5 * Time.deltaTime;
+            if(_currentEnergy >= _maxEnergy)
+            {
+                _currentEnergy = _maxEnergy;
+                _charging = false;
+                Fire();
+            }
+        }
+    }
+
+    private void Fire()
+    {
+        print("fire!");
+        _currentEnergy = 0;
     }
 }
